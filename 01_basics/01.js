40b0147abf11cbd0ce41db20e8ce8d598ca5468b
Md Atif Ansari <156583828+jdlxAKRON@users.noreply.github.com>
2025-12-23 09:48:41 +0000

    add some extra codes

--- 01_basics/01.js
+++ 01_basics/01.js
@@ -1,2 +1,7 @@
 console.log("hello");
 
+let name = "Akron"
+console.log(name);
+
+name= `Atif`
+console.log(name)
---
+++ 01_basics/02Arrow.js
@@ -0,0 +1,49 @@
+// const user ={
+//     username: "hitesh",
+//     price: "999",
+
+//     wellcomeMessage: function(){
+//         console.log(`${this.username}, welcome to the website`);
+        
+
+//     }
+
+// }
+
+// user.wellcomeMessage()
+// user.username="Akron"
+// user.wellcomeMessage()
+
+
+
+
+// function chai(){
+//     console.log(`Hello`);
+    
+// }
+
+// chai()
+
+
+// (function chai(){
+//     console.log(`Hello`);
+    
+// })()
+
+// (function chai(name){
+//     console.log(`Hello ${name}`);
+    
+// })(`Akron`)
+
+(function chai(name){
+    // named IIFE
+    console.log(`Hello ${name}`);
+    
+})(`Akron`);
+
+(function (){
+    // simple IIFE
+    console.log(`hello`);
+    
+})()
+
